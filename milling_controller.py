f1dcedadfc33a2dbb9f25c6e77d63139ae310d35
Logan <lpr1m3@icloud.com>
2024-08-14 16:48:06 -0400

    added delay in tracking code

--- milling_controller.py
+++ milling_controller.py
@@ -290,10 +290,9 @@ class BinaryProgram:
             self.set_rgb('blue')
 
         if not self.dry_run:
-            if self.smoothed_detected2:  # smoothed_detected is a low-pass filtered detection
-                self.chassis.set_velocity(0, 90, -2)
-                time.sleep(0.2)
+            if self.smoothed_detected2:  # smoothed_detected is a low-pass filtered detection                
                 self.chassis.set_velocity(100, 90, self.red_position * 2)  # Control robot movement function
+                time.sleep(0.1)
                         # linear speed 50 (0~100), direction angle 90 (0~360), yaw angular speed 0 (-2~2)
             # elif self.smoothed_detected and self.smoothed_detected2:
             #     self.chassis.set_velocity(100, 90, 0)
