01e7616746483f3a56fb46640d41f08a0a9e22af
kenblu24 <kenblu24@hotmail.com>
2024-09-09 23:01:33 -0400

    better mappings

--- snn_milling_controller.py
+++ snn_milling_controller.py
@@ -34,7 +34,7 @@ b2oh = bool_to_one_hot
 
 
 class SNNMillingProgram(BinaryProgram):
-    neuro_tpc = 1
+    neuro_tpc = 2
 
     def __init__(self,
         dry_run: bool = False,
@@ -48,8 +48,8 @@ class SNNMillingProgram(BinaryProgram):
     ) -> None:
         super().__init__(dry_run, board, lab_cfg_path, servo_cfg_path, pause, False, exit_on_stop)
 
-        self.encoders = [ende.RateEncoder(self.neuro_tpc, [0.0, 1.0]) for _ in range(2)]
-        self.decoders = [ende.RateDecoder(self.neuro_tpc, [0.0, 1.0]) for _ in range(4)]
+        self.encoders = [ende.RateEncoder(self.neuro_tpc, [0.0, 2.0]) for _ in range(2)]
+        self.decoders = [ende.RateDecoder(self.neuro_tpc, [0.0, 2.0]) for _ in range(4)]
 
         self.boolean_detection_averager = st.Average(2)
 
@@ -96,14 +96,18 @@ class SNNMillingProgram(BinaryProgram):
         self.run(self.neuro_tpc)
         # v0, v1, w0, w1 = self.decode_output()
         data = self.decode_output()
-
-        v = 0.157030957542 * data[1] - 0.141815737164 * data[0]
-        w = 0.866455820451 * data[3] - 1.336446211942 * data[2]
+        data = [int(x) for x in data]
+        # three bins. One for +v, -v, omega.
+        v_mapping = [0.0, 0.141815737164, 0.157030957542]
+        w_mapping = [0.0, 0.866455820451, 1.336446211942]
+        v = v_mapping[data[1]] - v_mapping[data[0]]
+        w = w_mapping[data[3]] - w_mapping[data[2]]
 
         # convert w from rad to deg
-        w_deg = math.degrees(w)
-        turn_power = math.copysign(st.fmap(abs(w), 59.6, 140.7, 0.333, 1), w)
-        fspd_power = math.copysign(st.fmap(abs(v), 0.262, 0.360, 50, 100), v)
+        w_rad = w
+        w = math.degrees(w_rad)
+        fspd_power = math.copysign(st.fmap(abs(v), 0.123, 0.360, 35, 100), v)
+        turn_power = math.copysign(st.fmap(abs(w), 54.54, 140.7, 0.1, 1), w)
 
         # print(v, w)
         self.set_rgb('green' if bool(self.detected) else 'red')
