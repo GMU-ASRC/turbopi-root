65bb8c953c43470c7b11fa9d3c5eac95a920f748
kenblu24 <kenblu24@hotmail.com>
2024-11-24 17:54:40 -0500

    fix graph_tsv labels, add slicing

--- hiwonder_common/src/hiwonder_common/graph_tsv.py
+++ hiwonder_common/src/hiwonder_common/graph_tsv.py
@@ -1,10 +1,12 @@
 import sys
-import argparse
-import pandas as pd
-from matplotlib import pyplot as plt
 import pathlib
+import argparse
 import colorsys
 from ast import literal_eval as eval
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
 try:
     from itertools import pairwise
@@ -25,49 +27,60 @@ def hr(h, s, l):  # noqa: E741
     return colorsys.hls_to_rgb(h, l, s)
 
 
-def graph(data):
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-    # read the csv files using pandas excluding the timedate column
-    # df = df.drop(columns=['time'], axis=1)
-    cr = hr(0.0, 0.9, 0.4)
-    cb = hr(0.6, 0.9, 0.4)
-    cg = hr(0.3, 0.9, 0.4)
+def get_last_move(moves):
+    try:
+        return moves[-1]
+    except IndexError:
+        return float('nan')
 
-    fig, ax = plt.subplots()
-    ax.cla()
 
-    def get_last_move(moves):
-        try:
-            return moves[-1]
-        except IndexError:
-            return float('nan')
+def convert_time_from_start(data, start=None):
+    tcol = data.iloc[:, 0].name
+    start = start or data[tcol][0]
+    data[tcol] -= start
+    # ts = times.copy() - times[0]
+    if tcol.strip().lower() == 'time_ns':
+        # cast to float and divide by 1e9 to get seconds
+        data = data.assign(**{tcol: data[tcol] / 1e9})
+        # ts /= 1e9
+    return data
 
-    times = data.iloc[:, 0]
-    ts = times.copy() - times[0]
-    if ts.name.strip().lower() == 'time_ns':
-        ts /= 1e9
+
+def slice_by_time(data, start=None, end=None, max_length=None):
+    a = data.iloc[0, 0] if start is None else start
+    b = data.iloc[-1, 0] if end is None else end
+    if max_length is not None:
+        if end is not None:
+            a = max(a, b - max_length)
+        else:
+            b = min(b, a + max_length)
+    ts = data.iloc[:, 0]
+    # https://stackoverflow.com/questions/38862657/find-value-greater-than-level-python-pandas
+    first_idx = np.argmax(ts >= a)  # returns 0 if not found
+    last_idx = np.argmax(ts >= b)
+    if first_idx == last_idx or a >= b:
+        raise ValueError("All data was excluded.")
+    return data.iloc[first_idx:last_idx]
+
+
+def get_moves(data):
+    ts = data.iloc[:, 0]
+    # ts = get_time_from_start(data)
 
     moves = data.iloc[:, -1]
     moves = moves.apply(eval)  # time consuming
     moves = moves.apply(get_last_move)
     moves = moves.apply(pd.Series, index=['v', 'd', 'w'])
+    v = moves['v']
+    w = moves['w']
 
-    if not moves['v'].empty:
-        # Plot the velocity
-        ax.plot(ts, moves['v'], label="Velocity", color="blue", alpha=0.5, linestyle="-")
-
-    if not moves['w'].empty:
-        # Plot the turn rate
-        axw = ax.twinx()
-        axw.plot(ts, moves['w'], label="Turn Rate", color="red", alpha=0.5, linestyle="-")
-
-    inputs = data.iloc[:, 1:-1]
     sense = None
+    inputs = data.iloc[:, 1:-1]
+
     if not inputs.empty:
         # create green vertical spanning regions for sensors
         sense = inputs.iloc[:, -1]
-        xsen = [ts[0]] if not sense.empty and sense[0] else []
+        xsen = [ts[0]] if not sense.empty and sense.iloc[0] else []
         xnot = []
         for (xi, si), (xn, sn) in pairwise(zip(ts, sense)):
             if sn > si:
@@ -77,41 +90,107 @@ def graph(data):
         if not sense.empty and sense.iloc[-1]:
             xnot.append(len(sense) - 1)
 
+    return ts, v, w, sense, xsen, xnot
+
+
+def graph(data):
+    plt.rcParams["figure.figsize"] = [7.00, 5.00]
+    # plt.rcParams["figure.autolayout"] = True
+    # read the csv files using pandas excluding the timedate column
+    # df = df.drop(columns=['time'], axis=1)
+    cr = hr(0.0, 0.9, 0.4)
+    cb = hr(0.6, 0.9, 0.4)
+    cg = hr(0.3, 0.9, 0.4)
+
+    fig, ax = plt.subplots()
+    ax.cla()
+    axw = ax.twinx()
+
+    ts, v, w, sense, xsen, xnot = get_moves(data)
+
+    if not v.empty:
+        # Plot the velocity
+        ax.plot(ts, v, label="Velocity", color="blue", alpha=0.5, linestyle="-")
+        ax.margins(0.1)
+
+    if not w.empty:
+        # Plot the turn rate
+        axw.plot(ts, w, label="Turn Rate", color="red", alpha=0.5, linestyle="-")
+        axw.margins(0.3)
+
+    if not sense.empty:
         # Plot the binary detection
         # ax.plot(ts, sense, label=sense.name, color="blue", linestyle="-", marker="o")
         ax.plot(ts, sense, c=cg, label=sense.name, alpha=0.1)
         # if plot_state:
         #     ax.subplot(111, aspect='equal')
         for xa, xb in zip(xsen, xnot):
-            ax.axvspan(xa, xb, ymin=0.0, ymax=1.0, alpha=0.15, color='green')
+            ax.axvspan(xa, xb, ymin=0.0, ymax=1.0, alpha=0.2, color='green')
 
     # Labels and Title
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("Output Values")
+
+    # make the legend
+    # grab the artists from the last row
+    handles, labels = ax.get_legend_handles_labels()
+    a, b = axw.get_legend_handles_labels()
+    handles.insert(-1, *a)
+    labels.insert(-1, *b)
+
+    # show the legend
+    legend = ax.legend(handles=handles, loc='lower center', ncol=3, fancybox=True, shadow=True)
+    # set the linewidth of each legend object
+    for obj in legend.legend_handles:
+        obj.set_linewidth(3.0)
+    s = legend.legend_handles[-1]
+    s.set_linewidth(10.0)
+    s.set_alpha(0.25)
+
     plt.title("Output Values over Time")
-    plt.legend()
+    ax.set_xlabel("Time since start (seconds)", loc='center')
+    ax.set_ylabel("Forward Velocity (m/s)")
+    axw.set_ylabel("Angular Velocity (rad/s)")
+    # plt.subplots_adjust(hspace=0.2, right=(1 - fig.subplotpars.left))
     plt.grid(True)
-    plt.show()
+    return plt
+
+
+def make_project(filename=None):
+    if not filename:
+        path = pathlib.Path(project.inquire_project())
+        return project.make_default_project(path)
+    elif not filename.is_file():
+        return project.make_default_project(filename, root='logs')
+
+
+def read_file(filename):
+    sep = '\t' if filename.suffix == '.tsv' else ','
+    return pd.read_csv(filename, sep=sep, skiprows=[], parse_dates=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=pathlib.Path, help="csv file to be graphed", nargs='?')
+    parser.add_argument("--offset", type=float, help="Number of seconds at the start of the file to skip", default=None)
+    parser.add_argument("--offset_end", type=float, help="Number of seconds at the end of the file to ignore", default=None)
+    parser.add_argument("--length", type=float, help="Length of time to graph in seconds", default=None)
     args = parser.parse_args()
 
     if project:
-        if not args.filename:
-            path = pathlib.Path(project.inquire_project())
-            p = project.make_default_project(path)
-        elif not args.filename.is_file():
-            p = project.make_default_project(args.filename, root='logs')
-        filename = p.root / 'io.tsv'
+        filename = make_project(args.filename).root / 'io.tsv'
     else:
         filename = args.filename
 
     if project and project.inquire_size(filename):
         sys.exit(1)
 
-    sep = '\t' if filename.suffix == '.tsv' else ','
-    data = pd.read_csv(filename, sep=sep, skiprows=[], parse_dates=True)
-    graph(data)
+    data = read_file(filename)
+    data = convert_time_from_start(data)
+    end = data.iloc[-1, 0] - args.offset_end if args.offset_end is not None else None
+    try:
+        data = slice_by_time(data, start=args.offset, end=end, max_length=args.length)
+    except ValueError:
+        print("No data was found in the specified range.")
+        print(f"File length: {data.iloc[-1, 0]} seconds")
+        sys.exit(1)
+    plt = graph(data)
+    plt.show()
