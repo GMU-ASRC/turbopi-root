fa9278d7cbbb33e83b6a0d4549295a02f275f1c9
Your Name <prabhusoham@outlook.com>
2025-07-07 14:28:06 -0400

    testing Diffusion

--- snn_milling_controller.py
+++ snn_milling_controller.py
@@ -17,7 +17,7 @@ import casPYan.ende.rate as ende
 # typing
 from typing import Any
 
-DEFAULT_NETWORK_PATH = '/home/pi/networks/240914-174037-best.json'
+DEFAULT_NETWORK_PATH = '/home/pi/networks/250507-103927-rss_snn_eons_diff-es26_n7.json'
 
 
 def bool_to_one_hot(x: bool):
