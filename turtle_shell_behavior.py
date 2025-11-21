6eadb85c975b0662b13cadd175fd5c692e30ea68
rvega10 <64618584+rvega10@users.noreply.github.com>
2025-11-21 16:46:17 -0500

    Simplify ShellingProgram by removing unused code
    Refactor ShellingProgram to use only CameraBinaryProgram_red and adjust movement logic.

--- turtle_shell_behavior.py
+++ turtle_shell_behavior.py
@@ -13,22 +13,29 @@ import hiwonder_common.camera_binary_program as camera_binary_program
 import hiwonder_common.camera_binary_program_red as camera_binary_program_red
 
 
-class ShellingProgram(camera_binary_program.CameraBinaryProgram,camera_binary_program_red.CameraBinaryProgram_red):
+# class ShellingProgram(camera_binary_program.CameraBinaryProgram,camera_binary_program_red.CameraBinaryProgram_red):
+class ShellingProgram(camera_binary_program_red.CameraBinaryProgram_red):
 
     def control(self):
-        self.set_rgb('green' if bool(self.smoothed_detected) else 'blue')
+        # self.set_rgb('green' if bool(self.smoothed_detected) else 'blue')
+        # self.set_rgb('red' if bool(self.smoothed_detected_red) else 'blue')
+        # if self.smoothed_detected_red:  # smoothed_detected is a low-pass filtered detection
+        #     self.move(100, 90, -0.5)  # Control robot movement function
+        # elif self.smoothed_detected:
+        #     self.move(100, 90, -0.25)
+        # else:
+        #     self.move(100, 90, 0.25)
+
         self.set_rgb('red' if bool(self.smoothed_detected_red) else 'blue')
         if self.smoothed_detected_red:  # smoothed_detected is a low-pass filtered detection
-            self.move(100, 90, -0.5)  # Control robot movement function
-        elif self.smoothed_detected:
-            self.move(100, 90, -0.25)
+            self.move(100, 90, 0)  # Control robot movement function
         else:
-            self.move(100, 90, 0.25)
+            self.move(100, 90, 0.5)
 
 
 def get_parser(parser, subparsers=None):
     return (
-        camera_binary_program.get_parser(parser, subparsers),
+        # camera_binary_program.get_parser(parser, subparsers),
         camera_binary_program_red.get_parser(parser, subparsers)
     )
 
@@ -40,5 +47,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     program = ShellingProgram(args)
-    camera_binary_program.main(args, program)
+    # camera_binary_program.main(args, program)
     camera_binary_program_red.main(args, program)
