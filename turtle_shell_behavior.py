e04a2e8459d2b3e3594870f32c928af97c121b02
rvega10 <64618584+rvega10@users.noreply.github.com>
2025-11-21 15:25:34 -0500

    Refactor control logic for turtle shell behavior

--- turtle_shell_behavior.py
+++ turtle_shell_behavior.py
@@ -18,15 +18,20 @@ class ShellingProgram(camera_binary_program.CameraBinaryProgram,camera_binary_pr
     def control(self):
         self.set_rgb('green' if bool(self.smoothed_detected) else 'blue')
         self.set_rgb('red' if bool(self.smoothed_detected_red) else 'blue')
-        if self.smoothed_detected:  # smoothed_detected is a low-pass filtered detection
+        if self.smoothed_detected_red:  # smoothed_detected is a low-pass filtered detection
             self.move(100, 90, -0.5)  # Control robot movement function
+        elif self.smoothed_detected:
+            self.move(100, 90, -0.25)
         else:
-            self.move(100, 90, 0.5)
+            self.move(100, 90, 0.25)
 
 
 def get_parser(parser, subparsers=None):
-    return camera_binary_program.get_parser(parser, subparsers)
-    return camera_binary_program_red.get_parser(parser, subparsers)
+    return (
+        camera_binary_program.get_parser(parser, subparsers),
+        camera_binary_program_red.get_parser(parser, subparsers)
+    )
+
 
 
 if __name__ == '__main__':
