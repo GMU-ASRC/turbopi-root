1604672fbde2eff387cc686d245429756aa76a26
HadleyPound-BENG <poundhadley@gmail.com>
2026-07-01 15:08:53 -0400

    Color changing LED functions

---
+++ hiwonder_common/src/hiwonder_common/color_change.py
@@ -0,0 +1,49 @@
+import serial
+import time
+
+SERIAL_PORT = 'COM3'
+BAUD = 115200
+
+#{ Color Index }# 
+red = b"255,0,0\n"
+green = b"0,255,0\n"
+blue = b"0,0,255\n"
+off = b"0,0,0\n"
+
+def change_color(color):
+    try:
+        ser = serial.Serial(SERIAL_PORT, BAUD)
+        time.sleep(0.1)
+
+        color.strip().lower()
+
+        if color == "red":
+            ser.write(red)
+            print("red info sent")
+            ser.close()
+
+        elif color == "green":
+            ser.write(green)
+            print("green info sent")
+            ser.close()
+
+        elif color == "blue":
+            ser.write(blue)
+            print("blue info sent")
+            ser.close()
+            
+        else: print("Invalid Color")
+
+    except serial.SerialException as e:
+        print(f"Error opening or communicating with the serial port: {e}")
+
+if __name__ == '__main__':
+    while True:
+        color = input("Color: ")
+        if color != "off":
+            change_color(color)
+        else:
+            ser = serial.Serial(SERIAL_PORT, BAUD)
+            ser.write(off)
+            ser.close()
+            break
