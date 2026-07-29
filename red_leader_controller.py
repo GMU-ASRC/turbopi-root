004b0ea718f6fb611ea22508319489a30fd07cfd
suhaybzed482k <suhaybzed@gmail.com>
2026-07-29 15:41:09 -0400

    add two species red leader green orbiter behaviour

---
+++ red_leader_controller.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# coding=utf8
+
+# pyright: reportImplicitOverride=false
+
+import argparse
+
+import hiwonder_common.camera_binary_program as camera_binary_program
+from hiwonder_common.color_change import ColorChange
+
+
+class RedLeaderProgram(camera_binary_program.CameraBinaryProgram):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.target_colors = ['red']
+        self.color = ColorChange()
+        self.color.change_color('red')
+
+    def control(self):
+        self.move(100, 90, 0.2)
+
+    
+def get_parser(parser, subparsers=None):
+    return camera_binary_program.get_parser(parser, subparsers)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    get_parser(parser)
+    args = parser.parse_args()
+
+    program = RedLeaderProgram(args)
+    camera_binary_program.main(args, program)
