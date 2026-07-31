98f3ed1a19324bdbd8b2521efd682cb221d1a7d8
suhaybzed482k <suhaybzed@gmail.com>
2026-07-31 16:27:56 -0400

    anthoner red turn update p3

--- red_leader_controller.py
+++ red_leader_controller.py
@@ -18,7 +18,7 @@ class RedLeaderProgram(camera_binary_program.CameraBinaryProgram):
         self.color.change_color('red')
 
     def control(self):
-        self.move(100, 90, 0.6)
+        self.move(100, 90, 0.5)
 
     
 def get_parser(parser, subparsers=None):
