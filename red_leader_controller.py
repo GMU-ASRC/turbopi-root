05960f6191e85f35ec4ac818442c2d35ae122f26
suhaybzed482k <suhaybzed@gmail.com>
2026-08-12 17:16:01 -0400

    sonar addition

--- red_leader_controller.py
+++ red_leader_controller.py
@@ -17,11 +17,38 @@ class RedLeaderProgram(camera_binary_program.CameraBinaryProgram):
         self.target_colors = ['red']
         self.color = ColorChange()
         self.color.change_color('red')
+
+        self.sonar = Sonar.Sonar()
+        self.distance = float('nan')
+        self.avoid_distance = 200  # mm, react before getting too close
+        self.avoid_timer = 0
+
         self.walk_timer = 0
         self.turn_timer = 0
         self.current_turn = 0.5
 
+    def read_distance(self):
+        distance = self.sonar.getDistance()
+        if round(distance) == 5000:
+            distance = float('inf')
+        elif distance > 5000:
+            distance = float('nan')
+        self.distance = distance
+
     def control(self):
+        self.read_distance()
+
+        if self.avoid_timer > 0:
+            self.move(-40, 90, self.current_turn)
+            self.avoid_timer -= 1
+            return
+
+        if self.distance < self.avoid_distance:
+            self.avoid_timer = 20
+            self.current_turn = random.choice([-0.8, 0.8])
+            self.move(-40, 90, self.current_turn)
+            return
+
         if self.walk_timer > 0:
             self.move(100, 90, 0)
             self.walk_timer -= 1
