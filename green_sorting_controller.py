eec286154d306e33a754d7668910fdac6570caef
michael-0954 <michaeljtikkala@gmail.com>
2026-08-12 11:33:14 -0400

    Add sorting behavior: species attract own color, repel other

---
+++ green_sorting_controller.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# coding=utf8
+
+# pyright: reportImplicitOverride=false
+
+import argparse
+
+import hiwonder_common.camera_binary_program as camera_binary_program
+from hiwonder_common.color_change import ColorChange
+
+
+class GreenSortingProgram(camera_binary_program.CameraBinaryProgram):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.target_colors = ['blue', 'green']
+        self.color = ColorChange()
+        self.color.change_color('green')
+
+    def control(self):
+        if self.smoothed_detected['blue']:
+            self.move(60, 90, 1.0)
+        elif self.smoothed_detected['green']:
+            self.move(60, 90, -0.5)
+        else:
+            self.move(60, 90, 0.5)
+
+
+def get_parser(parser, subparsers=None):
+    return camera_binary_program.get_parser(parser, subparsers)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    get_parser(parser)
+    args = parser.parse_args()
+
+    program = GreenSortingProgram(args)
+    camera_binary_program.main(args, program)
