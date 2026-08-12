f4e014039a3ff1a46e0194acf689aa5e6a31b4cf
suhaybzed482k <suhaybzed@gmail.com>
2026-08-12 15:16:45 -0400

    second main version

--- green_orbiter_controller.py
+++ green_orbiter_controller.py
@@ -13,15 +13,18 @@ class GreenOrbiterProgram(camera_binary_program.CameraBinaryProgram):
 
     def __init__(self, args):
         super().__init__(args)
-        self.target_colors = ['red']
+        self.target_colors = ['red', 'green']
         self.color = ColorChange()
         self.color.change_color('green')
 
     def control(self):
-        if self.smoothed_detected['red']:
-            self.move(100, 90, -0.5)
+        if self.smoothed_detected['red'] and not self.smoothed_detected['green']:
+            self.move(100, 90, -0.5) #sees red with no crowding so orbit normally
+        elif self.smoothed_detected['red'] and self.smoothed_detected['green']:
+            #half speed so it falls behind since it sees red and green so its too close to another orbiter
+            self.move(50, 90, -0.5)
         else:
-            self.move(100, 90, 0.5)
+            self.move(30, 90, 1.5) #slow down and turn faster to reaquire red
 
 
 def get_parser(parser, subparsers=None):
